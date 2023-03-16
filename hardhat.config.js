17b7b4c3f4e8e4473790e494eee6fb7db99b5722
MD-Dhaval-Saxena <122987863+MD-Dhaval-Saxena@users.noreply.github.com>
2023-03-16 12:31:51 +0530

    #udpated

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,8 +7,8 @@ module.exports = {
    defaultNetwork: "goerli",
   networks: {
     goerli: {
-      url: "https://goerli.infura.io/v3/138c213db28a4fa1845efe932a110e02",
-      accounts:["a1bcb37d90f2491cbe2509555fedbdabbc3b0cf129a726b2683c9e8265d6546e"]
+      url: "infur url here",
+      accounts:["Metamask private key"]
     },
   },
 };
