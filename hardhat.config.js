48745fdcb9993bd7d7cca5cab88df1eb1e45ff7a
Dhaval saxena <dhaval.saxena@minddeft.net>
2023-03-16 13:08:41 +0530

    env

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.5.16",
-   defaultNetwork: "goerli",
+   defaultNetwork: "sepolia",
   networks: {
-    goerli: {
-      url: "https://goerli.infura.io/v3/138c213db28a4fa1845efe932a110e02",
-      accounts:["a1bcb37d90f2491cbe2509555fedbdabbc3b0cf129a726b2683c9e8265d6546e"]
+    sepolia: {
+      url: process.env.INFURA_URL,
+      accounts:[process.env.API_KEY]
     },
   },
 };
